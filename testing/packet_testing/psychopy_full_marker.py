b28f775dc00d852263db322c845c6df20a49062d
ZackGoldblum <zackgoldblum@gmail.com>
2022-05-09 10:24:52 -0400

    Revised full marker script

--- testing/packet_testing/psychopy_full_marker.py
+++ testing/packet_testing/psychopy_full_marker.py
@@ -44,15 +44,12 @@ udp_socket_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR , 1)
 udp_socket_receive.bind(("", psychopy_PORT)) 
 
-# Timestamp testing
-
-psychopy_sent_timestamp_list = []
-psychopy_recv_timestamp_list = []
-psychopy_sent_micros_list = []
-psychopy_recv_micros_list = []
-raw_kernel_packets_list = []
+# One byte testing
 
 num_packets = 0
+psychopy_sent_micros_list = []
+psychopy_recv_micros_list = []
+t_start = micros()
 while num_packets < 1000:
     timestamp = time.time()
     data_to_send = {
@@ -67,50 +64,20 @@ while num_packets < 1000:
     psychopy_sent_micros_list.append(micros())
     data_bytes, addr = udp_socket_receive.recvfrom(1024)
     if data_bytes:
-        psychopy_recv_timestamp_list.append(time.time_ns())
         psychopy_recv_micros_list.append(micros())
-        raw_kernel_packets_list.append(data_bytes)
         num_packets += 1
-    psychopy_sent_timestamp_list.append(sent_timestamp)
 
-kernel_recv_timestamp_list = []
-kernel_sent_timestamp_list = []
+bytes_df = pd.DataFrame(list(zip(psychopy_sent_micros_list, psychopy_recv_micros_list)), columns=["Sent (us)", "Recieved (us)"])
+bytes_df.loc[:, "Two-way time (us)"] = bytes_df.loc[:, "Recieved (us)"].subtract(bytes_df.loc[:, "Sent (us)"])
+bytes_df.loc[:, "One-way time (us)"] = bytes_df.loc[:, "Two-way time (us)"].divide(2)
 
-for kernel_packet in raw_kernel_packets_list:
-    kernel_data = kernel_packet.decode("utf-8") 
-    kernel_recv_timestamp_list.append(float(kernel_data.split(",")[0]))
-    kernel_sent_timestamp_list.append(float(kernel_data.split(",")[1]))
+print("Num packets: ", len(bytes_df))
+print("One-way Mean (ms): ", round(bytes_df["One-way time (us)"].mean()/1000, 4))
+print("One-way STD (ms): ", round(bytes_df["One-way time (us)"].std()/1000, 4))
 
-bytes_timestamps_df = pd.DataFrame(list(zip(psychopy_sent_micros_list, psychopy_recv_micros_list)), columns=["Sent (us)", "Recieved (us)"])
-bytes_timestamps_df.loc[:, "Two-way time (us)"] = bytes_timestamps_df.loc[:, "Recieved (us)"].subtract(bytes_timestamps_df.loc[:,"Sent (us)"])
-bytes_timestamps_df.loc[:, "One-way time (us)"] = bytes_timestamps_df.loc[:, "Two-way time (us)"].divide(2)
-bytes_timestamps_df["PsychoPy Sent (UTC)"] = psychopy_sent_timestamp_list
-bytes_timestamps_df["PsychoPy Recv (UTC)"] = psychopy_recv_timestamp_list
-bytes_timestamps_df["Kernel Sent (UTC)"] = kernel_sent_timestamp_list
-bytes_timestamps_df["Kernel Recv (UTC)"] = kernel_recv_timestamp_list
-
-print("Num packets: ", len(bytes_timestamps_df))
-one_way_mean_us = bytes_timestamps_df["One-way time (us)"].mean()
-print("One-way Mean (ms): ", round(one_way_mean_us/1000, 4))
-one_way_std_us = bytes_timestamps_df["One-way time (us)"].std()
-print("One-way Std (ms): ", round(one_way_std_us/1000, 4))
-
-one_way_mean_s = one_way_mean_us * 1e6
-
-# T2-(T1+t1)
-bytes_timestamps_df["Clock delta (us)"] = bytes_timestamps_df.loc[:, "Kernel Recv (UTC)"].subtract(bytes_timestamps_df.loc[:, "PsychoPy Sent (UTC)"].add(one_way_mean_s))
-bytes_timestamps_df["Clock delta (us)"] = bytes_timestamps_df.loc[:, "Clock delta (us)"].divide(1e6)
-
-# If negative, Kernel clock is behind
-# If positive, Kernel clock is ahead
-clock_delta_mean = bytes_timestamps_df["Clock delta (us)"].mean()
-print("Clock-delta Mean (ms): ", round(clock_delta_mean/1000, 4))
-clock_delta_std = bytes_timestamps_df["Clock delta (us)"].std()
-print("Clock-delta Std (ms): ", round(clock_delta_std/1000, 4))
-
-filename = "packet_bytes_timestamps_df.xlsx"
+filename = "packet_bytes_df.xlsx"
 filepath = os.path.join(os.getcwd(), filename)
 
 writer = pd.ExcelWriter(filepath)
-bytes_timestamps_df.to_excel(writer)
+bytes_df.to_excel(writer)
 writer.save()
