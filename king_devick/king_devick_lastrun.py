f104ccb92b6cf6cc77eba186c92e1dca1c57f1e6
ZackGoldblum <zackgoldblum@gmail.com>
2022-05-18 09:46:35 -0400

    Added data folders to gitignore file

--- king_devick/king_devick_lastrun.py
+++ king_devick/king_devick_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on May 17, 2022, at 10:11
+    on May 18, 2022, at 09:44
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -62,7 +62,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\Sim\\Desktop\\KernelFlow_PsychoPy\\king_devick\\king_devick_lastrun.py',
+    originPath='C:\\Users\\zackg\\OneDrive\\Ayaz Lab\\KernelFlow_PsychoPy\\king_devick\\king_devick_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
