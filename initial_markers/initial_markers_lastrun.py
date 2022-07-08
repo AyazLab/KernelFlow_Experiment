0763a1428b144915bf7e3045b69006f6ab98ff62
ZackGoldblum <zackgoldblum@gmail.com>
2022-07-08 14:27:48 -0400

    Added initial markers experiment

---
+++ initial_markers/initial_markers_lastrun.py
@@ -0,0 +1,268 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
+    on July 08, 2022, at 14:16
+If you publish work using this script the most relevant publication is:
+
+    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
+        PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
+        https://doi.org/10.3758/s13428-018-01193-y
+
+"""
+
+from __future__ import absolute_import, division
+
+from psychopy import locale_setup
+from psychopy import prefs
+from psychopy import sound, gui, visual, core, data, event, logging, clock, colors
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle, choice as randchoice
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+from psychopy.hardware import keyboard
+
+from datetime import datetime
+import time
+import os
+import sys
+import csv
+
+initial_timestamp = time.time()
+clock_time_offset = logging.defaultClock.getTime()
+task_start_timestamp = initial_timestamp - clock_time_offset  # account for timestamp delay from clock creation
+
+task_start_timestamp_fmt = int(task_start_timestamp * 1e9)
+start_time = datetime.fromtimestamp(task_start_timestamp).strftime("%Y-%m-%d-%H-%M-%S-%f")
+
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2021.2.3'
+expName = 'initial_markers'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath='C:\\Users\\Sim\\Desktop\\KernelFlow_PsychoPy\\initial_markers\\initial_markers_lastrun.py',
+    savePickle=True, saveWideText=False,
+    dataFileName=filename)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+frameTolerance = 0.001  # how close to onset before 'same' frame
+
+# Start Code - component code to be run after the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=[1920, 1080], fullscr=True, screen=0, 
+    winType='pyglet', allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[-0.7000, -0.7000, -0.7000], colorSpace='rgb',
+    blendMode='avg', useFBO=True, 
+    units='height')
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# Setup eyetracking
+ioDevice = ioConfig = ioSession = ioServer = eyetracker = None
+
+# create a default keyboard (e.g. to check for escape)
+defaultKeyboard = keyboard.Keyboard()
+
+# Initialize components for Routine "initial_exp_code"
+initial_exp_codeClock = core.Clock()
+win.mouseVisible = False
+
+# setup markers -----
+cwd = os.getcwd()
+kernel_socket_path = os.path.join(os.path.dirname(cwd), "main", "kernel_socket")
+sys.path.insert(0, kernel_socket_path)
+from kernel_socket import Initial_Marker
+marker = Initial_Marker()
+
+# start experiment marker -----
+marker.send_marker(101, task_start_timestamp_fmt)
+
+# Initialize components for Routine "instructions"
+instructionsClock = core.Clock()
+instructions_text = visual.TextStim(win=win, name='instructions_text',
+    text='Re-run this experiment until Kernel receives a marker.\n\nPress SPACE to end.',
+    font='Open Sans',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+instructions_resp = keyboard.Keyboard()
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# ------Prepare to start Routine "initial_exp_code"-------
+continueRoutine = True
+# update component parameters for each repeat
+# keep track of which components have finished
+initial_exp_codeComponents = []
+for thisComponent in initial_exp_codeComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+initial_exp_codeClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "initial_exp_code"-------
+while continueRoutine:
+    # get current time
+    t = initial_exp_codeClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=initial_exp_codeClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in initial_exp_codeComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "initial_exp_code"-------
+for thisComponent in initial_exp_codeComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# the Routine "initial_exp_code" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "instructions"-------
+continueRoutine = True
+# update component parameters for each repeat
+instructions_resp.keys = []
+instructions_resp.rt = []
+_instructions_resp_allKeys = []
+# keep track of which components have finished
+instructionsComponents = [instructions_text, instructions_resp]
+for thisComponent in instructionsComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+instructionsClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "instructions"-------
+while continueRoutine:
+    # get current time
+    t = instructionsClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=instructionsClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *instructions_text* updates
+    if instructions_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        instructions_text.frameNStart = frameN  # exact frame index
+        instructions_text.tStart = t  # local t and not account for scr refresh
+        instructions_text.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(instructions_text, 'tStartRefresh')  # time at next scr refresh
+        instructions_text.setAutoDraw(True)
+    
+    # *instructions_resp* updates
+    if instructions_resp.status == NOT_STARTED and t >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        instructions_resp.frameNStart = frameN  # exact frame index
+        instructions_resp.tStart = t  # local t and not account for scr refresh
+        instructions_resp.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(instructions_resp, 'tStartRefresh')  # time at next scr refresh
+        instructions_resp.status = STARTED
+        # keyboard checking is just starting
+        instructions_resp.clock.reset()  # now t=0
+    if instructions_resp.status == STARTED:
+        theseKeys = instructions_resp.getKeys(keyList=['space'], waitRelease=False)
+        _instructions_resp_allKeys.extend(theseKeys)
+        if len(_instructions_resp_allKeys):
+            instructions_resp.keys = _instructions_resp_allKeys[-1].name  # just the last key pressed
+            instructions_resp.rt = _instructions_resp_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in instructionsComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "instructions"-------
+for thisComponent in instructionsComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# the Routine "instructions" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+# end experiment marker -----
+task_end_timestamp = time.time() - clock_time_offset
+task_end_timestamp_fmt = int(task_end_timestamp * 1e9)
+marker.send_marker(102, task_end_timestamp_fmt)
+
+# Flip one final time so any remaining win.callOnFlip() 
+# and win.timeOnFlip() tasks get executed before quitting
+win.flip()
+
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsPickle(filename)
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
--- main/kernel_socket/kernel_socket.py
+++ main/kernel_socket/kernel_socket.py
@@ -51,7 +51,7 @@ class Marker():
 
     def make_marker_dict(self, marker_dict_path):
         df = pd.read_csv(self.marker_dict_path)
-        marker_vals =df["marker_val"]
+        marker_vals = df["marker_val"]
         marker_strs = df["marker_str"]
 
         marker_dict = {}
@@ -61,9 +61,9 @@ class Marker():
         return marker_dict
 
     def send_marker(self, marker_val, timestamp):
-        if str(marker_val)[1] == "1":
+        if str(marker_val)[-1] == "1":
             marker_str = "start_experiment"
-        elif str(marker_val)[1] == "2":
+        elif str(marker_val)[-1] == "2":
             marker_str = "end_experiment"
         else:
             marker_str = "ERROR"
@@ -83,3 +83,70 @@ class Marker():
         exp_marker_str = self.marker_dict[str(marker_val)]
 
         logging.log(msg=f"UDP Sent: marker_ID={marker_ID}, marker_value={marker_val}, marker_string={exp_marker_str}, timestamp={timestamp}", level=logging.EXP)
+
+class Initial_Marker():
+    def __init__(self):
+        self.opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_config_path = self.get_socket_path()
+        self.marker_dict_path = self.get_marker_dict_path()
+        self.marker_dict = self.make_marker_dict(self.marker_dict_path)
+        self.kernel_IP, self.kernel_PORT = self.get_socket_info(self.socket_config_path)
+
+    def get_socket_path(self): 
+        cwd = os.getcwd()
+        temp_path = cwd.split("\\KernelFlow_PsychoPy")[0]
+        socket_config_path = os.path.join(temp_path, "KernelFlow_PsychoPy", "main", "kernel_socket", "socket_config.json")
+
+        return socket_config_path
+
+    def get_socket_info(self, socket_config_path):
+        socket_config = open(socket_config_path)
+        socket_data = json.load(socket_config)
+        socket_config.close()
+
+        kernel_IP = socket_data["kernel_IP"]
+        kernel_PORT = socket_data["kernel_PORT"]
+
+        return kernel_IP, kernel_PORT
+
+    def get_marker_dict_path(self):
+        cwd = os.getcwd()
+        temp_path = cwd.split("\\KernelFlow_PsychoPy")[0]
+        marker_dict_path = os.path.join(temp_path, "KernelFlow_PsychoPy", "main", "marker_dict.csv")
+
+        return marker_dict_path
+
+    def make_marker_dict(self, marker_dict_path):
+        df = pd.read_csv(self.marker_dict_path)
+        marker_vals = df["marker_val"]
+        marker_strs = df["marker_str"]
+
+        marker_dict = {}
+        for marker_val, marker_str in zip(marker_vals, marker_strs):
+            marker_dict[str(marker_val)] = marker_str
+
+        return marker_dict
+
+    def send_marker(self, marker_val, timestamp):
+        if str(marker_val)[-1] == "1":
+            marker_str = "start_experiment"
+        elif str(marker_val)[-1] == "2":
+            marker_str = "end_experiment"
+        else:
+            marker_str = "ERROR"
+
+        marker_ID = 1
+        
+        data_to_send = {
+            "id": marker_ID,
+            "timestamp": timestamp,
+            "event": marker_str,
+            "value": str(marker_val)
+        }
+
+        print(data_to_send)
+        
+        event = json.dumps(data_to_send).encode("utf-8")
+        self.opened_socket.sendto(event, (self.kernel_IP, self.kernel_PORT))
+
+        exp_marker_str = self.marker_dict[str(marker_val)]
